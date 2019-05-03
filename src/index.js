a84cb0aeb3049bc624fcf163a565b9831e079799
Alex Ander <blagirev.a.v@gmail.com>
2019-05-03 13:33:41 +0300

    css added

--- src/index.js
+++ src/index.js
@@ -1,24 +1,33 @@
-import React from 'react'
-import ReactDOM from 'react-dom'
+import React from 'react';
+import ReactDOM from 'react-dom';
+
 import AppHeader from './components/app-header';
 import SearchPanel from './components/search-panel';
-import ToDoList from './components/todo-list';
+import TodoList from './components/todo-list';
+import ItemStatusFilter from './components/item-status-filter';
+
+import './index.css';
 
 const App = () => {
 
-    const toDoData = [
-        {label: 'Drink Coffee ASAP', important: false, id: 1},
-        {label: 'Build React App', important: true, id: 2},
-        {label: 'Have a good lunch', important: false, id: 3},
-    ]
+  const todoData = [
+    { label: 'Drink Coffee', important: false, id: 1 },
+    { label: 'Make Awesome App', important: true, id: 2 },
+    { label: 'Have a lunch', important: false, id: 3 }
+  ];
 
-    return (
-        <div>
-            <AppHeader />
-            <SearchPanel />
-            <ToDoList todos={toDoData}/>
-        </div>
-    )
-}
+  return (
+    <div className="todo-app">
+      <AppHeader toDo={1} done={3} />
+      <div className="top-panel d-flex">
+        <SearchPanel />
+        <ItemStatusFilter />
+      </div>
 
-ReactDOM.render(<App />, document.getElementById('root'))
+      <TodoList todos={todoData} />
+    </div>
+  );
+};
+
+ReactDOM.render(<App />,
+  document.getElementById('root'));
